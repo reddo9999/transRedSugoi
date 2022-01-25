cd442e0585f8ea2a959a9e79443d6e83e237ba77
reddo9999 <26098031+reddo9999@users.noreply.github.com>
2022-01-25 18:26:24 -0300

    add no-japanese detector

--- ts/classes/RedStringEscaper.ts
+++ ts/classes/RedStringEscaper.ts
@@ -260,6 +260,9 @@ class RedStringEscaper {
             }
             if (newReg) formulas.push(newReg);
         }
+
+        // Since sugoi only translates japanese, might as well remove anything else
+        formulas.push(/(^[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf])/g);
         RedStringEscaper.cachedFormulaString = JSON.stringify(sys.config.escaperPatterns);
         RedStringEscaper.cachedFormulas = formulas;
         return formulas;
