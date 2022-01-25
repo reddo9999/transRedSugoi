176b7a90e796b3bd0d96b662d53a8f1b9edcb04a
reddo9999 <26098031+reddo9999@users.noreply.github.com>
2022-01-25 18:43:39 -0300

    k that was a bad idea let's just remember it

--- transredsugoi.js
+++ transredsugoi.js
@@ -217,7 +217,6 @@ class RedStringEscaper {
             if (newReg)
                 formulas.push(newReg);
         }
-        formulas.push(/(^[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf])/g);
         RedStringEscaper.cachedFormulaString = JSON.stringify(sys.config.escaperPatterns);
         RedStringEscaper.cachedFormulas = formulas;
         return formulas;
--- ts/classes/RedStringEscaper.ts
+++ ts/classes/RedStringEscaper.ts
@@ -267,7 +267,7 @@ class RedStringEscaper {
         }
 
         // Since sugoi only translates japanese, might as well remove anything else
-        formulas.push(/(^[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf])/g);
+        //formulas.push(/(^[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf])/g);
         RedStringEscaper.cachedFormulaString = JSON.stringify(sys.config.escaperPatterns);
         RedStringEscaper.cachedFormulas = formulas;
         return formulas;
