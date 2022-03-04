ab6df4eb110da591934183e92f2d474adc24d4fd
reddo9999 <26098031+reddo9999@users.noreply.github.com>
2022-03-04 16:36:23 -0300

    Same improvement for text inside colors

--- ts/classes/RedTranslatorEngine.ts
+++ ts/classes/RedTranslatorEngine.ts
@@ -27,9 +27,7 @@ const defaultIsolateRegexp =
     `)` +
     `[${openerRegExp}]([^${openerRegExp + closerRegExp}])+[${closerRegExp}]` +
 `)|(` +
-    `(?<=${rmColorRegExp})` +
-        `.+?` +
-    `(?=${rmColorRegExp})` +
+    `${rmColorRegExp}.+?${rmColorRegExp}` +
 `)`;
     
 
